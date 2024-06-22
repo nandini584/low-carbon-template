77c1cdbdf20787f0f9b5f1e091e2d0d9e70bf644
root <root@MyWorkspace>
2024-06-22 14:32:33 +0530

    Wagtail and Next.js setup done with tailwind CSS

---
+++ sustainablesite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-3qkg+^i#47(rh^g%u*wi&40digzx9cunyq&+93w5lvrm+$n9tx"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
