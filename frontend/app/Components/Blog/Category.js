0fc3007d8c69318cd3efde5281b6ae37720d82ec
root <root@MyWorkspace>
2024-07-26 13:55:36 +0530

    blogIndexpage ready

---
+++ frontend/app/Components/Blog/Category.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Category = () => {
+  return (
+    <div className='px-8 py-3 border border-4 rounded-br-xl border-[#490ED9]'>
+        <h1 className='text-[#490ED9] font-bold text-xl'>Sustainability</h1>
+    </div>
+  )
+}
+
+export default Category
