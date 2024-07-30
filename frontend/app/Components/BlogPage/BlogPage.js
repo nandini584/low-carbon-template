70882f3b64c6c16f30e104fe4abd20fb7e4d9ff7
nandini584 <nandini.arora.che22@itbhu.ac.in>
2024-07-30 11:34:18 +0530

    related blogs completed

--- frontend/app/Components/BlogPage/BlogPage.js
+++ frontend/app/Components/BlogPage/BlogPage.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import BlogHeader from './BlogHeader'
 import BlogContent from './BlogContent'
+import RelatedBlogs from './RelatedBlogs'
 
 const BlogPage = () => {
   return (
     <div>
         <BlogHeader/>
         <BlogContent/>
+        <RelatedBlogs/>
     </div>
   )
 }
--- frontend/app/Components/BlogPage/RelatedBlogs.js
+++ frontend/app/Components/BlogPage/RelatedBlogs.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import BlogCard from '../BlogIndexPage/BlogCard'
 
 const RelatedBlogs = () => {
   return (
-    <div>
-        
+    <div className="bg-[#EEEDEF] pl-36 py-16 mt-10">
+        <h2 className='text-black text-3xl font-semibold mb-10'>Related Blogs</h2>
+        <div className="flex flex-row flex-wrap gap-6">
+          <BlogCard />
+          <BlogCard />
+          <BlogCard />
+        </div>
     </div>
   )
 }
