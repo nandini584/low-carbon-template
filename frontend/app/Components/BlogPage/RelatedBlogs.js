2f67c3fdfc65ed2f5168ffdd997ee2b34e807ca4
nandini584 <nandini.arora.che22@itbhu.ac.in>
2024-08-05 18:11:57 +0530

    blog content, blog header and related blogs made responsive

--- frontend/app/Components/BlogPage/RelatedBlogs.js
+++ frontend/app/Components/BlogPage/RelatedBlogs.js
@@ -4,16 +4,16 @@ import BlogCard from '../BlogIndexPage/BlogCard';
 const RelatedBlogs = () => {
   return (
     <aside
-      className="bg-[#EEEDEF] pl-36 py-16 mt-10"
+      className="bg-[#EEEDEF] md:pl-36 px-6 md:px-0 md:py-16 py-6 md:mt-10 mt-6"
       aria-labelledby="related-blogs-title"
     >
       <h2
-        className="text-black text-3xl font-semibold mb-10"
+        className="text-black md:text-3xl text-2xl font-semibold md:mb-10 mb-6"
         id="related-blogs-title"
       >
         Related Blogs
       </h2>
-      <div className="flex flex-row flex-wrap gap-6">
+      <div className="flex md:flex-row flex-col flex-wrap md:gap-6 gap-4">
         <BlogCard />
         <BlogCard />
         <BlogCard />
