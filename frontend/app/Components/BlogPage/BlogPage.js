a9cc7b18226e4a8c4b560d6f413bdd62446a3f23
nandini584 <nandini.arora.che22@itbhu.ac.in>
2024-08-05 16:53:04 +0530

    removed the unneccesary div tags

--- frontend/app/Components/BlogPage/BlogPage.js
+++ frontend/app/Components/BlogPage/BlogPage.js
@@ -5,11 +5,11 @@ import RelatedBlogs from './RelatedBlogs';
 
 const BlogPage = () => {
   return (
-    <main>
+    <>
       <BlogHeader />
       <BlogContent />
       <RelatedBlogs />
-    </main>
+    </>
   );
 };
 
