6adc99e5d028f598481677a6d45d69ad3262319f
nandini584 <nandini.arora.che22@itbhu.ac.in>
2024-08-05 17:31:54 +0530

    added aria-labels and roles

--- frontend/app/Components/BlogPage/BlogHeader.js
+++ frontend/app/Components/BlogPage/BlogHeader.js
@@ -3,7 +3,7 @@ import React from 'react';
 const BlogHeader = () => {
   return (
     <div className="bg-[#F4F4F4] pl-36 pr-96 pt-20">
-      <h1 className="text-5xl text-black">
+      <h1 className="text-5xl text-black" id="blog-title">
         Sustainability Web Designs <br />{' '}
         <span className="text-[#490ED9] font-semibold">
           Accessible & best SEO practices
--- frontend/app/Components/BlogPage/RelatedBlogs.js
+++ frontend/app/Components/BlogPage/RelatedBlogs.js
@@ -3,14 +3,14 @@ import BlogCard from '../BlogIndexPage/BlogCard';
 
 const RelatedBlogs = () => {
   return (
-    <div className="bg-[#EEEDEF] pl-36 py-16 mt-10">
+    <aside className="bg-[#EEEDEF] pl-36 py-16 mt-10" role="complementary" aria-labelledby="related-blogs-title">
       <h2 className="text-black text-3xl font-semibold mb-10">Related Blogs</h2>
       <div className="flex flex-row flex-wrap gap-6">
         <BlogCard />
         <BlogCard />
         <BlogCard />
       </div>
-    </div>
+    </aside>
   );
 };
 
