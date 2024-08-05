469ab32bfca7a09502263af81ae2b9d539b1c8ab
nandini584 <nandini.arora.che22@itbhu.ac.in>
2024-08-05 18:02:38 +0530

    blog page responsive

--- frontend/app/Components/BlogIndexPage/BlogIndexPage.js
+++ frontend/app/Components/BlogIndexPage/BlogIndexPage.js
@@ -5,15 +5,15 @@ import Categories from './Categories';
 export default function BlogIndexPage() {
   return (
     <main className="bg-light_gray lg:px-36 px-6 lg:py-20 py-5">
-      <h1 className="lg:text-6xl text-2xl text-black">
+      <h1 className="lg:text-6xl text-2xl font-bold text-black">
         Sustainability Web Designs <br />{' '}
-        <span className="text-blue_violet font-semibold">
+        <span className="text-blue_violet font-bold">
           Accessible & best SEO <br /> practices
         </span>
       </h1>
       <div className="flex md:flex-row flex-col justify-between lg:mt-24 mt-10">
         <Categories />
-        <section className="flex flex-row flex-wrap gap-8 md:w-[45vw]">
+        <section className="flex flex-row flex-wrap gap-8 md:w-[45vw] mt-10 md:mt-0">
           <BlogCard />
           <BlogCard />
           <BlogCard />
--- frontend/app/Components/BlogIndexPage/Categories.js
+++ frontend/app/Components/BlogIndexPage/Categories.js
@@ -3,14 +3,14 @@ import Category from './Category';
 
 const Categories = () => {
   return (
-    <nav className="w-[30vw]" aria-labelledby="categories-heading">
+    <nav className="md:w-[30vw]" aria-labelledby="categories-heading">
       <h5
         className="text-gray-700 md:text-lg text-sm font-semibold mb-5"
         id="categories-heading"
       >
         Categories
       </h5>
-      <div className="flex flex-row gap-4 flex-wrap">
+      <div className="flex flex-row md:gap-4 gap-2 flex-wrap">
         <Category />
         <Category />
         <Category />
