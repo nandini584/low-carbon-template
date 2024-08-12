d51da2e193ee840ae3e74edac4485453a3febedf
nandini584 <nandini.arora.che22@itbhu.ac.in>
2024-08-12 17:10:27 +0530

    responsive components added

--- frontend/app/Components/HomePage/HeroSection.js
+++ frontend/app/Components/HomePage/HeroSection.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 const HeroSection = () => {
   return (
-    <div className="bg-[#F4F4F4] md:pl-36 pl-6 md:pr-96 md:py-20 py-4 flex flex-row items-center justify-between">
+    <div className="bg-[#F4F4F4] md:pl-36 px-6 md:pr-96 md:py-20 py-4 flex flex-row items-center justify-between">
         <div>
             <h1 className="md:text-6xl text-2xl text-black font-semibold">
                 Sustainability Web Designs <br />{' '}
@@ -11,13 +11,13 @@ const HeroSection = () => {
                 Accessible & best SEO <br/> practices
                 </span>
             </h1>
-            <h4 className='md:text-lg text-black text-sm mt-8 md:w-[45vw]'>
+            <h4 className='md:text-lg text-black text-sm md:mt-8 mt-4 md:w-[45vw] w-full '>
                 Our approach to web design is rooted in minimalism and sustainability, ensuring that every site we build is optimized for energy efficiency and low environmental impact.
             </h4>
-            <ul className='text-lg text-black flex flex-row item-center justify-between mt-8 w-[45vw] font-semibold'>
-                <li><span className='text-2xl'>&#x1f4af;</span> Sustainability</li>
-                <li><span className='text-2xl'>&#x1f4af;</span> Accessibility</li>
-                <li><span className='text-2xl'>&#x1f4af;</span> SEO</li>
+            <ul className='text-lg text-black flex md:flex-row flex-col item-center justify-between md:mt-8 mt-6 w-[45vw] font-semibold'>
+                <li><span className='md:text-2xl text-xl'>&#x1f4af;</span> Sustainability</li>
+                <li><span className='md:text-2xl text-x'>&#x1f4af;</span> Accessibility</li>
+                <li><span className='md:text-2xl text-x'>&#x1f4af;</span> SEO</li>
             </ul>
         </div>
       {/* <Image
