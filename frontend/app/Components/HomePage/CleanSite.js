117f7b42799a1135fbde2cb6b8cd3eaa6334f452
nandini584 <nandini.arora.che22@itbhu.ac.in>
2024-08-12 17:03:16 +0530

    components added

--- frontend/app/Components/HomePage/CleanSite.js
+++ frontend/app/Components/HomePage/CleanSite.js
@@ -2,7 +2,21 @@ import React from 'react'
 
 const CleanSite = () => {
   return (
-    <div>CleanSite</div>
+    <div className='bg-[#EEEDEF] md:pl-36 pl-6 md:pr-96 md:py-20 py-4 flex flex-row items-center justify-between'>
+        {/* <Image
+        src="/path/to/your/image.jpg"
+        alt="Description of the image"
+        width={500}
+        height={300}
+        /> */}
+        <div>
+            <h1 className='text-[#490ED9] md:text-3xl text-2xl font-bold'>Crafting Clean, Efficient <br/> Websites for a Greener Future</h1>
+            <h4 className='md:text-lg text-black text-sm my-8 md:w-[31vw]'>
+                Our approach to web design is rooted in minimalism and sustainability, ensuring that every site we build is optimized for energy efficiency and low environmental impact.
+            </h4>
+            <button className='text-black bg-[#D6F7E7] py-2 px-10 rounded-md border border-[3px] border-black text-lg '>Get Access to Latest Blogs</button>
+        </div>
+    </div>
   )
 }
 
--- frontend/app/Components/HomePage/LatestBlogs.js
+++ frontend/app/Components/HomePage/LatestBlogs.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import BlogCard from '../BlogIndexPage/BlogCard'
 
 const LatestBlogs = () => {
   return (
-    <div>LatestBlogs</div>
+    <div className="bg-[#E2DCF2] md:pl-36 px-6 md:px-0 md:py-16 py-6 md:pt-16 pt-6">
+      <h2 className="text-black md:text-3xl text-2xl font-semibold md:mb-16 mb-6">
+        Our Latest Blogs
+      </h2>
+      <div className="flex md:flex-row flex-col flex-wrap md:gap-12 gap-4">
+        <BlogCard/>
+        <BlogCard/>
+        <BlogCard/>
+      </div>
+    </div>
   )
 }
 
