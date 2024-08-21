24f77e25f32a9d524da4e5a4750a2695fd54e4ef
nandini584 <nandini.arora.che22@itbhu.ac.in>
2024-08-21 18:04:39 +0530

    categories added

--- blog/models.py
+++ blog/models.py
@@ -3,10 +3,14 @@ from django.db import models
 
 from modelcluster.fields import ParentalKey, ParentalManyToManyField
 from wagtail.models import Page, Orderable
-from wagtail.fields import RichTextField
+from wagtail.fields import RichTextField, StreamField
 from wagtail.admin.panels import FieldPanel, InlinePanel, MultiFieldPanel
 from wagtail.search import index
+from wagtail import blocks
+from modelcluster.contrib.taggit import ClusterTaggableManager
+from taggit.models import TaggedItemBase
 from wagtail.snippets.models import register_snippet
+from wagtail.images.blocks import ImageChooserBlock
 
 class BlogIndexPage(Page):
     main_title = models.CharField(max_length=255, default="Sustainable Development")
@@ -15,9 +19,14 @@ class BlogIndexPage(Page):
         FieldPanel('main_title', help_text="The page title as you'd like it to be seen by the public")
     ]
 
-    # Only allow BlogPages beneath this page.
     subpage_types = ["blog.BlogPage"]
 
+class BlogPageTag(TaggedItemBase):
+    content_object = ParentalKey(
+        'BlogPage',
+        related_name='tagged_items',
+        on_delete=models.CASCADE
+    )
 
 class BlogPage(Page):
     # Different from the real publication date, for editorial control.
@@ -26,6 +35,13 @@ class BlogPage(Page):
     introduction = RichTextField(max_length=250)
     # body = RichTextField(blank=True)
     authors = ParentalManyToManyField('blog.Author', blank=True)
+
+    body = StreamField([
+        ('image', ImageChooserBlock()),
+        ('paragraph', blocks.RichTextBlock(max_length=255)),
+    ], default=[("body","default")])
+
+    tags = ClusterTaggableManager(through=BlogPageTag, blank=True)
 
     def main_image(self):
         gallery_item = self.gallery_images.first()
@@ -37,23 +53,22 @@ class BlogPage(Page):
     search_fields = Page.search_fields + [
         index.SearchField('blog_title'),
         index.SearchField('introduction'),
-        # index.SearchField('body'),
+        index.SearchField('body'),
     ]
 
     content_panels = Page.content_panels + [
         MultiFieldPanel([
             FieldPanel('date'),
             FieldPanel('authors', widget=forms.CheckboxSelectMultiple),
+            FieldPanel('tags')
         ], heading="Blog information"),
         FieldPanel('blog_title', help_text="The page title as you'd like it to be seen by the public"),
-        # FieldPanel('date', help_text="This date will be shown on pages"),
         FieldPanel('introduction'),
-        # FieldPanel('body'),
+        FieldPanel('body'),
 
         InlinePanel('gallery_images', label="Gallery images"),
     ]
 
-    # Only allow this page to be created beneath a BlogIndexPage.
     parent_page_types = ["blog.BlogIndexPage"]
 
 
