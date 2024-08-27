0ae8e8521aab2b15640b67dd53d68a2ab9e6a0cd
nandini584 <nandini.arora.che22@itbhu.ac.in>
2024-08-27 14:11:35 +0530

    color codes switched to colornames

--- frontend/app/Components/HomePage/CleanSite.js
+++ frontend/app/Components/HomePage/CleanSite.js
@@ -1,23 +1,29 @@
-import React from 'react'
+import React from 'react';
 
 const CleanSite = () => {
   return (
-    <div className='bg-[#EEEDEF] md:pl-36 px-6 md:pr-96 md:py-20 py-10 flex flex-row items-center justify-between'>
-        {/* <Image
+    <div className="bg-lavendar_gray md:pl-36 px-6 md:pr-96 md:py-20 py-10 flex flex-row items-center justify-between">
+      {/* <Image
         src="/path/to/your/image.jpg"
         alt="Description of the image"
         width={500}
         height={300}
         /> */}
-        <div>
-            <h1 className='text-[#490ED9] md:text-3xl md:text-2xl text-xl font-bold'>Crafting Clean, Efficient <br/> Websites for a Greener Future</h1>
-            <h4 className='md:text-lg text-black text-sm md:my-8 my-6 md:w-[31vw]'>
-                Our approach to web design is rooted in minimalism and sustainability, ensuring that every site we build is optimized for energy efficiency and low environmental impact.
-            </h4>
-            <button className='text-black bg-[#D6F7E7] py-2 px-10 rounded-md border border-[3px] border-black text-lg '>Get Access to Latest Blogs</button>
-        </div>
+      <div>
+        <h1 className="text-blue_violet md:text-3xl md:text-2xl text-xl font-bold">
+          Crafting Clean, Efficient <br /> Websites for a Greener Future
+        </h1>
+        <h4 className="md:text-lg text-black text-sm md:my-8 my-6 md:w-[31vw]">
+          Our approach to web design is rooted in minimalism and sustainability,
+          ensuring that every site we build is optimized for energy efficiency
+          and low environmental impact.
+        </h4>
+        <button className="text-black bg-[#D6F7E7] py-2 px-10 rounded-md border border-[3px] border-black text-lg ">
+          Get Access to Latest Blogs
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default CleanSite
+export default CleanSite;
--- frontend/app/Components/HomePage/HeroSection.js
+++ frontend/app/Components/HomePage/HeroSection.js
@@ -1,25 +1,34 @@
-import React from 'react'
-import Image from 'next/image'
+import React from 'react';
+import Image from 'next/image';
 
 const HeroSection = () => {
   return (
-    <div className="bg-[#F4F4F4] md:pl-36 px-6 md:pr-96 md:py-20 py-4 flex flex-row items-center justify-between">
-        <div>
-            <h1 className="md:text-6xl text-2xl text-black font-semibold">
-                Sustainability Web Designs <br />{' '}
-                <span className="text-[#490ED9] font-semibold">
-                Accessible & best SEO <br/> practices
-                </span>
-            </h1>
-            <h4 className='md:text-lg text-black text-sm md:mt-8 mt-4 md:w-[45vw] w-full '>
-                Our approach to web design is rooted in minimalism and sustainability, ensuring that every site we build is optimized for energy efficiency and low environmental impact.
-            </h4>
-            <ul className='text-lg text-black flex md:flex-row flex-col item-center justify-between md:mt-8 mt-6 w-[45vw] font-semibold'>
-                <li><span className='md:text-2xl text-xl'>&#x1f4af;</span> Sustainability</li>
-                <li><span className='md:text-2xl text-x'>&#x1f4af;</span> Accessibility</li>
-                <li><span className='md:text-2xl text-x'>&#x1f4af;</span> SEO</li>
-            </ul>
-        </div>
+    <div className="bg-light_gray md:pl-36 px-6 md:pr-96 md:py-20 py-4 flex flex-row items-center justify-between">
+      <div>
+        <h1 className="md:text-6xl text-2xl text-black font-semibold">
+          Sustainability Web Designs <br />{' '}
+          <span className="text-blue_violet font-semibold">
+            Accessible & best SEO <br /> practices
+          </span>
+        </h1>
+        <h4 className="md:text-lg text-black text-sm md:mt-8 mt-4 md:w-[45vw] w-full ">
+          Our approach to web design is rooted in minimalism and sustainability,
+          ensuring that every site we build is optimized for energy efficiency
+          and low environmental impact.
+        </h4>
+        <ul className="text-lg text-black flex md:flex-row flex-col item-center justify-between md:mt-8 mt-6 w-[45vw] font-semibold">
+          <li>
+            <span className="md:text-2xl text-xl">&#x1f4af;</span>{' '}
+            Sustainability
+          </li>
+          <li>
+            <span className="md:text-2xl text-x">&#x1f4af;</span> Accessibility
+          </li>
+          <li>
+            <span className="md:text-2xl text-x">&#x1f4af;</span> SEO
+          </li>
+        </ul>
+      </div>
       {/* <Image
         src="/path/to/your/image.jpg"
         alt="Description of the image"
@@ -27,7 +36,7 @@ const HeroSection = () => {
         height={300}
         /> */}
     </div>
-  )
-}
+  );
+};
 
-export default HeroSection
+export default HeroSection;
